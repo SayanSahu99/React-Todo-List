65a277c7abcd46b1954b46f6ddd1e71c32838224
SayanSahu99 <sayansovan@gmail.com>
2020-07-04 23:53:08 +0530

    Capturing the input from the form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,42 @@ import uuid from 'uuid';
 import 'font-awesome/css/font-awesome.min.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      id: uuid(),
+      item: '',
+      editItem: false
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      item: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      item: this.state.item
+    };
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuid(),
+      editItem: false
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -14,7 +50,7 @@ class App extends Component {
         <div className="col-10 col-md-8 mx-auto mt-4">
             <Header />
             <h3 className="text-capitalize text-center">enter tasks</h3>
-            <TodoInput />
+            <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
             <TodoList />
         </div>
       </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,16 +3,20 @@ import { Input, Form, Card, CardBody, InputGroup, Button } from 'reactstrap'
 
 export default class TodoInput extends Component {
     render() {
+        const {item, handleChange, handleSubmit} = this.props;
         return (
             <Card>
                 <CardBody>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <InputGroup>
                             <div className="input-group-prepend">
                                 <div className="input-group-text text-white bg-secondary"><i className="fa fa-book"/></div>
                             </div>
                             <Input type="text" id="todo-input" name="todo-input"
-                                    className="form-control text-capitalize" placeholder="add a task" />
+                                    className="form-control text-capitalize" placeholder="add a task"
+                                    value={item}
+                                    onChange={handleChange}
+                                    />
                             <Button type="submit" className="btn mt-3 btn-block text-capitalize">add task</Button>
                         </InputGroup>
 
